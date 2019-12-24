871b0e35f6606ddd4357a1eb1bba301cacb15a13
Tasos Kourtis <tasoskour1@gmail.com>
2019-12-24 03:05:33 +0200

    backend clean

--- login-app/backend/server.js
+++ login-app/backend/server.js
@@ -1,7 +1,6 @@
 const express=require("express");
 const cors=require("cors");
 const mongoose=require("mongoose");
-//let User=require("../backend/models/users.model.js");
 
 require('dotenv').config();
 
@@ -22,25 +21,6 @@ connection.once('open',()=>{
   console.log("Connection to mongodb successfull")
 });
 
-/*
-//GET Users
-app.route('/users').get((req,res)=>{
-User.find()
-.then(users=>res.json(users))
-.catch(err=>res.status(400).json('Error'+err));
-});
-
-//POST Users
-app.route('/users/add').post((req,res)=>{
-const username=req.body.username;
-const newUser= new User({username});
-
-newUser.save()
-  .then(()=>res.json('User added'))
-  .catch(err=>res.status(400).json('Error'+err))
-});
-
-*/
 
 const usersRouter =require("./routes/users")
 app.use('/users',usersRouter);
